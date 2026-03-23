1d8cfbaca7bac23fcd51fc4a409f2f5d96750439
kaushikbat7-design <kaushik.bat7@gmail.com>
2026-03-23 15:04:08 +0530

    UC4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,24 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        String greeting;
+
+        String[] args = new String[0];
+        if (args.length > 0) {
+            StringBuilder names = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                if (i > 0) {
+                    names.append(", ");
+                }
+                names.append(args[i]);
+            }
+            greeting = "Hello, " + names.toString() + "!";
+        } else {
+            greeting = "Hello, World!";
         }
+
+        System.out.println(greeting);
+
     }
 }
